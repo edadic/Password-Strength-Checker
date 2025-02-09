7d9938b7a619ecc11d668225dcb208b18a29fb47
Eldar Dadić <eldardadic@Eldars-MacBook-Air.local>
2025-02-10 00:34:58 +0100

    js script moved into a seperate file

---
+++ static/script.js
@@ -0,0 +1,18 @@
+function checkPassword() {
+    var password = $("#password").val();
+    $.ajax({
+        url: "/check",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify({ password: password }),
+        success: function(response) {
+            $("#result")
+                .html(response.message)
+                .css("color", response.strength === 5 ? "green" : response.strength >= 3 ? "orange" : "red");
+
+            var feedbackHtml =
+                "<ul>" + response.feedback.map(f => "<li>" + f + "</li>").join("") + "</ul>";
+            $("#feedback").html(feedbackHtml);
+        }
+    });
+}
